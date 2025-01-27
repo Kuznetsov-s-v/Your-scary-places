3d69ba5ea0615def4189e9319e8f825d4cec09de
sergey <kuznetsov_s_v@vk.com>
2025-01-28 00:35:08 +0300

    Initial Commit

--- app/src/main/java/com/bloodmoon/yourscaryplaces/ui/screens/planets/views/PlanetPageViewDisplay.kt
+++ app/src/main/java/com/bloodmoon/yourscaryplaces/ui/screens/planets/views/PlanetPageViewDisplay.kt
@@ -1,7 +1,6 @@
 package com.bloodmoon.yourscaryplaces.ui.screens.planets.views
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
